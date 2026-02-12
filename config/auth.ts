36e396e9073a72f5cc9ca626e4deeb78bf334f85
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2026-02-12 22:09:25 +0200

    jwt

--- config/auth.ts
+++ config/auth.ts
@@ -1,18 +1,33 @@
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-
-export const authConfig: AuthOptions ={
+export const authConfig: AuthOptions = {
+    // Без адаптера - використовуємо JWT
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
     ],
+    session: {
+        strategy: "jwt" as const,
+    },
     callbacks: {
+        async jwt({token, user, account}) {
+            if (user) {
+                token.id = user.id;
+                token.email = user.email;
+                token.name = user.name;
+                token.picture = user.image;
+            }
+            return token;
+        },
         async session({session, token}) {
-            if (session.user) {
-                session.user.id = token.sub!;
+            if (token && session.user) {
+                session.user.id = token.id as string;
+                session.user.email = token.email as string;
+                session.user.name = token.name as string;
+                session.user.image = token.picture as string;
             }
             return session;
         },
@@ -25,5 +40,6 @@ export const authConfig: AuthOptions ={
     },
     pages: {
         signIn: "/",
-    }
-} 
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+}
